294c068a04d6fd3bf26fbeff7754f0049ddad308
Maxim Mihin <maxim.mihin@outlook.com>
2024-12-21 03:03:15 +0300

    change module name

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -33,7 +33,7 @@ import (
 	"github.com/getkin/kin-openapi/openapi3"
 	"golang.org/x/tools/imports"
 
-	"github.com/oapi-codegen/oapi-codegen/v2/pkg/util"
+	"github.com/maximmihin/oapi-codegen/v2/pkg/util"
 )
 
 // Embed the templates directory
